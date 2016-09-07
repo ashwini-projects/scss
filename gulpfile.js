c1fe0eb9a8c259f84fc056cc3fe1117f95c1b591
ashwini <ashwini1081@yahoo.com>
2016-09-07 22:28:05 +0530

    stable working version

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,29 @@
 let gulp = require('gulp');
 let scss = require('gulp-scss');
+let sass = require('gulp-sass');
+
+
+gulp.task('sass', () => {
+  return gulp.src("style/**/*.scss")
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('style/css/'))
+});
+
+gulp.task('sass:watch', () => {
+  gulp.watch("style/scss/**/*.scss", ['sass']);
+  gulp.watch("*.js", ['sass']);
+});
 
 gulp.task('scss', () => {
-
+  gulp.src(
+    "style/scss/**/*.scss"
+    )
+    .pipe(scss(
+      {"bundleExec": true}
+    ))
+    .pipe(gulp.dest("style/css"));
 });
+
+// gulp.task('default', ['watch', 'scss', 'serve']);
+gulp.task('default', ['sass:watch', 'sass']);
+
